262ac497c385d7efd55cb5c8f35b819a62159f47
nmcb <marco@splatter.nl>
2022-09-22 21:48:07 +0200

    solved day 5

---
+++ src/main/scala/Day05.scala
@@ -0,0 +1,53 @@
+import scala.io._
+
+object Day05 extends App:
+
+  val start1: Long =
+    System.currentTimeMillis
+
+  val strings: List[String] =
+    Source
+      .fromFile("src/resources/input05.txt")
+      .getLines
+      .toList
+
+  val answer1: Int =
+    val filter = for {
+      str <- strings
+      if str.count("aeiou".toList.contains) >= 3
+      if str.sliding(2).map(_.toArray).count(cs => cs(0) == cs(1)) >= 1
+      if str.sliding(2).count(cs => cs == "ab" || cs == "cd" || cs == "pq" || cs == "xy") == 0
+    } yield str
+    filter.size
+
+  println(s"Answer part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+
+  val start2: Long =
+    System.currentTimeMillis
+
+  val answer2: Int =
+    def containsTwoOrMoreNonOverlappingPair(str: String): Boolean =
+      def hasNonOverlappingPair(pair: String, todo: String, foundFirst: Boolean = false): Boolean =
+        val index = todo.indexOf(pair)
+        if      (index != -1 && !foundFirst) hasNonOverlappingPair(pair, todo.drop(index + 2), true)
+        else if (index != -1 &&  foundFirst) true
+        else                                 false
+
+      (str.sliding(2).distinct).exists(pair => hasNonOverlappingPair(pair, str)) 
+
+    def containsTwoSameCharsOneCharApart(str: String): Boolean =
+      def hasTwoSameCharsOneCharApart(todo: List[Char]): Boolean =
+        todo match
+          case c0 ::  _ :: c2 :: tail if c0 == c2 => true
+          case c0 :: c1 :: c2 :: tail             => hasTwoSameCharsOneCharApart(c1 :: c2 :: tail)
+          case _                                  => false
+      hasTwoSameCharsOneCharApart(str.toList)
+
+    val filter = for {
+      str <- strings
+      if containsTwoOrMoreNonOverlappingPair(str)
+      if containsTwoSameCharsOneCharApart(str)
+    } yield str
+    filter.size
+  
+  println(s"Answer part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
