18d9f9f65334914f299b0ae9a42e038777d50172
nmcb <marco@splatter.nl>
2022-09-30 21:38:28 +0200

    optimised performance

--- src/main/scala/Day06.scala
+++ src/main/scala/Day06.scala
@@ -2,65 +2,88 @@ import scala.io.*
 
 object Day06 extends App:
 
-  val start1: Long =
-    System.currentTimeMillis
+  /** input */
 
-  case class Pos(x: Int, y: Int):
-    assert(x >= 0 && x <= 999)
-    assert(y >= 0 && y <= 999)
+  case class Task(cmd: String, rec: Rec)
 
-  case class Rec(p0: Pos, p1: Pos):
-    assert(p0.x <= p1.x)
-    assert(p0.y <= p1.y)
+  val input: List[Task] =
+    def parser(s: String): Task =
+      s match
+        case s"toggle $p0x,$p0y through $p1x,$p1y"   => Task("toggle", Rec(p0x, p0y, p1x, p1y))
+        case s"turn on $p0x,$p0y through $p1x,$p1y"  => Task("on"    , Rec(p0x, p0y, p1x, p1y))
+        case s"turn off $p0x,$p0y through $p1x,$p1y" => Task("off"   , Rec(p0x, p0y, p1x, p1y))
 
-    def contains(p: Pos): Boolean =
-      p.x >= p0.x && p.x <= p1.x && p.y >= p0.y && p.y <= p1.y
-
-  case class Task(command: String, rec: Rec)
-
-  case class Sol[A](lights: Map[Pos,A]):
-
-    def run(toggle: A => A, on: A => A, off: A => A)(task: Task): Sol[A] =
-      task match
-        case Task("toggle", rec) => copy(lights = lights.map((p,l) => if rec.contains(p) then (p, toggle(l)) else (p, l)))
-        case Task("on",     rec) => copy(lights = lights.map((p,l) => if rec.contains(p) then (p,     on(l)) else (p, l)))
-        case Task("off",    rec) => copy(lights = lights.map((p,l) => if rec.contains(p) then (p,    off(l)) else (p, l)))
-        case _ => sys.error("boom!")
-
-  object Sol:
-    def empty[A](init: => A): Sol[A] =
-      Sol(List.tabulate(1000, 1000)((x,y) => Pos(x,y) -> init).flatten.toMap)
-
-  val tasks: List[Task] =
     Source
       .fromFile("src/resources/input06.txt")
       .getLines
+      .map(parser)
       .toList
-      .map {
-        case s"toggle $p0x,$p0y through $p1x,$p1y" => Task("toggle", Rec(Pos(p0x.toInt, p0y.toInt), Pos(p1x.toInt, p1y.toInt)))
-        case s"turn on $p0x,$p0y through $p1x,$p1y" => Task("on", Rec(Pos(p0x.toInt, p0y.toInt), Pos(p1x.toInt, p1y.toInt)))
-        case s"turn off $p0x,$p0y through $p1x,$p1y" => Task("off", Rec(Pos(p0x.toInt, p0y.toInt), Pos(p1x.toInt, p1y.toInt)))
-      }
 
-  val answer1: Int =
-    var c = 0
-    tasks.foldLeft(Sol.empty(false))((sol,task) => {
-      println(c)
-      c = c + 1
-      sol.run(l => !l, _ => true, _ => false)(task)
-    }).lights.filter(_._2).size
+  /** modeling */
 
-  println(s"Answer part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+  case class Rec(x0: Int, y0: Int, x1: Int, y1: Int):
+    def contains(x: Int, y: Int): Boolean =
+      x >= x0 && x <= x1 && y >= y0 && y <= y1
 
-  val start2: Long =
-    System.currentTimeMillis
+  object Rec:
+    def apply(p0x: String, p0y: String, p1x: String, p1y: String): Rec =
+      Rec(p0x.toInt, p0y.toInt, p1x.toInt, p1y.toInt)
 
-  val answer2: Int =
-    var c = 0
-    tasks.foldLeft(Sol.empty(0))((sol,task) => {
-      println(c)
-      c = c + 1
-      sol.run(l => l + 2, l => l + 1, l => if (l - 1) >= 0 then l - 1 else 0)(task)
-    }).lights.values.sum
+  case class Light[A](value: A):
+    def exec(cmd: Command[A]): Light[A] =
+      Light(cmd.op(value))
+
+  case class Command[A](op: A => A)
+
+  sealed trait Commands[A]:
+    val init: Light[A]
+    val toggle: Command[A]
+    val on: Command[A]
+    val off: Command[A]
+
+    private def exec(tasks: List[Task])(x: Int, y: Int): A =
+      tasks
+        .filter(_.rec.contains(x, y))
+        .map(_.cmd match
+          case "toggle" => toggle
+          case "on"     => on
+          case "off"    => off)
+        .foldLeft(init)(_ exec _)
+        .value
+
+    def exec(tasks: List[Task]): List[A] =
+      List.tabulate(1000, 1000)(exec(tasks)).flatten
+
+
+  /** Part 1 */
+
+  object BooleanLights extends Commands[Boolean]:
+    val init   = Light(false)
+    val toggle = Command(v => !v)
+    val on     = Command(_ => true)
+    val off    = Command(_ => false)
+
+  val start1: Long = System.currentTimeMillis
+  val answer1: Int = BooleanLights.exec(input).map(on => if on then 1 else 0).sum
+
+  println(s"Answer part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
+
+
+  /** Part 2 */
+
+  object IntLights extends Commands[Int]:
+    val init   = Light(0)
+    val toggle = Command(v => v + 2)
+    val on     = Command(v => v + 1)
+    val off    = Command(v => if v <= 0 then 0 else v - 1)
+
+  val start2: Long = System.currentTimeMillis
+  val answer2: Int = IntLights.exec(input).sum
 
   println(s"Answer part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
+
+
+  /** Solution assertions */
+
+  assert(answer1 ==   569999, s"Wrong answer part 1: $answer1")
+  assert(answer2 == 17836115, s"Wrong answer part 2: $answer2")
