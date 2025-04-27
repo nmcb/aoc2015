7df17979045384ab43dd7cb794f7395442aec1d9
nmc.borst <marco@splatter.nl>
2025-04-27 11:15:13 +0200

    solved day 19 (#62)
    * Update scala3-library to 3.4.1
    
    * declared next method as infix
    
    * name scala build
    
    * performance enhancements day 04
    
    * fixed build
    
    * solved day 19
    
    ---------
    
    Co-authored-by: nmcb-scala-steward[bot] <157481287+nmcb-scala-steward[bot]@users.noreply.github.com>

--- src/main/scala/Day19.scala
+++ src/main/scala/Day19.scala
@@ -1,71 +1,56 @@
+import scala.io.Source
+
 object Day19 extends App:
 
-  import scala.io.*
 
   val day: String =
-    this.getClass.getName.drop(3).init
+    getClass.getName.drop(3).init
 
-  /** Modeling */
-
-  import Day12.*
-  import P.*
-
-  object Molecules:
-
-    private val upper: P[Char] =
-      satisfy(_.isUpper)
-
-    private val lower: P[Char] =
-      satisfy(_.isLower)
-
-    private val molecule: P[String] =
-      for { c1 <- upper ; c2 <- lower.zeroOrMore } yield (c1 :: c2).mkString
-
-    val parser: P[List[String]] =
-      molecule.oneOrMore
-
-
-  /** Input */
-
-  val molecules: List[String] =
-    Molecules
-      .parser
-      .run("HOH")
-//      .run("CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCa" +
-//           "CaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMg" +
-//           "ArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTi" +
-//           "BPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFAr" +
-//           "CaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMg" +
-//           "ArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRn" +
-//           "BPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgAr" +
-//           "SiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCa" +
-//           "PTiBPTiBCaSiThSiRnMgArCaF")
-
-  val replacements: Map[String,List[List[String]]] =
+  val molecules: String =
     Source
       .fromResource(s"input$day.txt")
       .getLines
-      .map { case s"$molecule => $replacement" => molecule -> Molecules.parser.run(replacement) }
-      .toList
-      .groupMap(_._1)(_._2)
+      .filterNot(_.isBlank)
+      .filterNot(_.contains("=>"))
+      .next
+
+  val replacements: Vector[(String,String)] =
+    Source
+      .fromResource(s"input$day.txt")
+      .getLines
+      .collect:
+        case s"$molecule => $replacement" => molecule -> replacement
+      .toVector
 
   /** Part 1 */
-
   val start1: Long =
     System.currentTimeMillis
 
   val answer1 =
-    666
+    var generations = Set.empty[String]
+    for from -> to <- replacements do
+      for index <- 0 until molecules.length do
+        if molecules.slice(index, index + from.length) == from then
+          val prefix  = molecules.substring(0, index)
+          val postfix = molecules.substring(index + from.length)
+          generations += (prefix + to + postfix)
+    generations.size
 
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
 
   /** Part 2 */
+  val start2: Long   = System.currentTimeMillis
 
-  val start2: Long =
-    System.currentTimeMillis
-
-  val answer2: Int =
-    669
+  /** Luck > Skill : replacements are already in the right order */
+  val answer2 =
+    var target = molecules
+    var count  = 0
+    while target != "e" do
+      for from -> to <- replacements do
+        if target.contains(to) then
+          target  = target.replaceFirst(to, from)
+          count = count + 1
+    count
 
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -2,85 +2,86 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestSolutions extends AnyFunSuite:
 
-  test("Day01 [3ms]"):
-    assertResult( 138)(actual = Day01.answer1)
+  test("Day01"):
+    assertResult(138)(actual = Day01.answer1)
     assertResult(1771)(actual = Day01.answer2)
 
-  test("Day02 [5ms]"):
+  test("Day02"):
     assertResult(1606483)(actual = Day02.answer1)
     assertResult(3842356)(actual = Day02.answer2)
 
-  test("Day03 [34ms]"):
+  test("Day03"):
     assertResult(2572)(actual = Day03.answer1)
     assertResult(2631)(actual = Day03.answer2)
 
-  test("Day04 [229s]"):
-    assertResult( 282749)(actual = Day04.answer1)
+  test("Day04"):
+    assertResult(282749)(actual = Day04.answer1)
     assertResult(9962624)(actual = Day04.answer2)
 
-  test("Day05 [21ms]"):
+  test("Day05"):
     assertResult(258)(actual = Day05.answer1)
-    assertResult( 53)(actual = Day05.answer2)
+    assertResult(53)(actual = Day05.answer2)
 
-  test("Day06 [4s]"):
-    assertResult(  569999)(actual = Day06.answer1)
+  test("Day06"):
+    assertResult(569999)(actual = Day06.answer1)
     assertResult(17836115)(actual = Day06.answer2)
 
-  test("Day07 [41ms]"):
-    assertResult(   956)(actual = Day07.answer1)
-    assertResult( 40149)(actual = Day07.answer2)
+  test("Day07"):
+    assertResult(956)(actual = Day07.answer1)
+    assertResult(40149)(actual = Day07.answer2)
 
-  test("Day08 [5ms]"):
+  test("Day08"):
     assertResult(1333)(actual = Day08.answer1)
     assertResult(2046)(actual = Day08.answer2)
 
-  test("Day09 [204ms]"):
+  test("Day09"):
     assertResult(207)(actual = Day09.answer1)
     assertResult(804)(actual = Day09.answer2)
 
-  ignore("Day10 [812s]"):
-    assertResult( 252594)(actual = Day10.answer1)
+  ignore("Day10"):
+    assertResult(252594)(actual = Day10.answer1)
     assertResult(3579328)(actual = Day10.answer2)
 
-  test("Day11 [2338ms]"):
+  test("Day11"):
     assertResult("hxbxxyzz")(actual = Day11.answer1)
     assertResult("hxcaabcc")(actual = Day11.answer2)
 
-  test("Day12 [12ms]"):
+  test("Day12"):
     assertResult(111754)(actual = Day12.answer1)
-    assertResult( 65402)(actual = Day12.answer2)
+    assertResult(65402)(actual = Day12.answer2)
 
-  test("Day13 [1724ms]"):
+  test("Day13"):
     assertResult(733)(actual = Day13.answer1)
     assertResult(725)(actual = Day13.answer2)
 
-  test("Day14 [20ms]"):
+  test("Day14"):
     assertResult(2660)(actual = Day14.answer1)
     assertResult(1256)(actual = Day14.answer2)
 
-  test("Day15 [313ms]"):
+  test("Day15"):
     assertResult(13882464)(actual = Day15.answer1)
     assertResult(11171160)(actual = Day15.answer2)
 
-  test("Day16 [2ms]"):
-    assertResult( 40)(actual = Day16.answer1)
+  test("Day16"):
+    assertResult(40)(actual = Day16.answer1)
     assertResult(241)(actual = Day16.answer2)
 
-  test("Day17 [1013ms]"):
-    assertResult( 4372)(actual = Day17.answer1)
-    assertResult(    4)(actual = Day17.answer2)
+  test("Day17"):
+    assertResult(4372)(actual = Day17.answer1)
+    assertResult(4)(actual = Day17.answer2)
 
-  test("Day18 [5133ms]"):
-    assertResult( 814)(actual = Day18.answer1)
-    assertResult( 924)(actual = Day18.answer2)
+  test("Day18"):
+    assertResult(814)(actual = Day18.answer1)
+    assertResult(924)(actual = Day18.answer2)
 
-  ignore("Day19 [???ms]"):
-    assertResult( 666)(actual = Day19.answer1)
-    assertResult( 666)(actual = Day19.answer2)
+  test("Day19"):
+    assertResult(535)(actual = Day19.answer1)
+    assertResult(212)(actual = Day19.answer2)
 
-  test("Day20 [3463ms]"):
-    assertResult( 831600)(actual = Day20.answer1)
-    assertResult( 884520)(actual = Day20.answer2)
-  test("Day21 [5ms]"):
-    assertResult( 111)(actual = Day21.answer1)
-    assertResult( 188)(actual = Day21.answer2)
+  test("Day20"):
+    assertResult(831600)(actual = Day20.answer1)
+    assertResult(884520)(actual = Day20.answer2)
+
+  test("Day21"):
+    assertResult(111)(actual = Day21.answer1)
+    assertResult(188)(actual = Day21.answer2)
